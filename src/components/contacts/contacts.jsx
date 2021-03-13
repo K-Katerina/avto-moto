99afffd53be00b217c031c77a3f06fadfdfeb023
Katerina <katerina_kw@mail.ru>
2021-03-13 21:13:03 +0300

    Заполнение и стили контактов

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -5,7 +5,33 @@ const Contacts = ({className}) => {
     return (
         <article className={`contact ${className}`}>
             <h2 className="visually-hidden contact__title">Контакты</h2>
-            Контакты
+            <ul className="contact__list contact-list">
+                <li className="contact-list__item">
+                    <span className="contact-list__txt">
+                        <b className="contact-list__title">Адрес</b>
+                        Санкт-Петербург, набережная&nbsp;реки&nbsp;Карповки,&nbsp;дом&nbsp;5
+                    </span>
+                </li>
+                <li className="contact-list__item">
+                    <span className="contact-list__txt">
+                        <b className="contact-list__title">Режим работы</b>
+                        Ежедневно, с&nbsp;10:00&nbsp;до&nbsp;21:00
+                    </span>
+                </li>
+                <li className="contact-list__item">
+                    <span className="contact-list__txt">
+                        <b className="contact-list__title">Телефон</b>
+                        <a className="contact-list__tel" href="tel:88003335599">8&nbsp;(800)&nbsp;333-55-99</a>
+                    </span>
+                </li>
+                <li className="contact-list__item">
+                    <span className="contact-list__txt">
+                        <b className="contact-list__title">E-mail</b>
+                        <a className="contact-list__email" href="mailto:info@avto-moto.ru">info@avto-moto.ru</a>
+                    </span>
+                </li>
+            </ul>
+            <div className="contact__map">Map</div>
         </article>
     );
 };
